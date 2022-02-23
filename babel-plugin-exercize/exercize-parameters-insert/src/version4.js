baf79c3c34c615f65ed0b1d5c0e298a0ecbd9215
lei hehl <lei.hehl@shopee.com>
2022-02-23 11:03:17 +0800

    fix: babel

---
+++ babel-plugin-exercize/exercize-parameters-insert/src/version4.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+const { transformFileSync } = require('@babel/core');
+const  parser = require('@babel/parser');
+const insertParametersPlugin = require('./plugin/parameters-insert-plugin');
+const fs = require('fs');
+const path = require('path');
+
+const { code } = transformFileSync(path.join(__dirname, './sourceCode.js'), {
+    plugins: [insertParametersPlugin],
+    parserOpts: {
+        sourceType: 'unambiguous',
+        plugins: ['jsx']       
+    }
+});
+
+console.log(code);
