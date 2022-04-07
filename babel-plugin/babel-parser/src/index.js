8b9c5f44c263342c7fc047a7afab463022815ce8
lei hehl <lei.hehl@shopee.com>
2022-04-07 23:47:29 +0800

    fix: add babel-parser

---
+++ babel-plugin/babel-parser/src/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+const parser = require('@babel/parser');
+const fs = require('fs');
+const path = require('path');
+
+const sourceCode = fs.readFileSync(path.join(__dirname, './sourceCode.js'), {
+  encoding: 'utf-8'
+});
+
+const ast = parser.parse(sourceCode, {
+  sourceType: 'unambiguous'
+});
+
+const astExpression = parser.parseExpression(sourceCode)
+
+console.log(ast);
+console.log(astExpression);
---
+++ babel-plugin/babel-parser/src/sourceCode.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import aa from 'aa';
+import * as bb from 'bb';
+import {cc} from 'cc';
+import 'dd';
+
+function a () {
+    console.log('aaa');
+}
+
+class B {
+    bb() {
+        return 'bbb';
+    }
+}
+
+const c = () => 'ccc';
+
+const d = function () {
+    console.log('ddd');
+}
+
