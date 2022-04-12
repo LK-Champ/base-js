345de96fb00d71da64c685c4f01606068c002554
lei hehl <lei.hehl@shopee.com>
2022-04-12 18:36:53 +0800

    fix: test

--- babel-plugin-exercize/exercize-auto-i18n/src/index.js
+++ babel-plugin-exercize/exercize-auto-i18n/src/index.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 const { transformFromAstSync } = require('@babel/core');
 const  parser = require('@babel/parser');
 const autoI18nPlugin = require('./plugin/auto-i18n-plugin');
 const fs = require('fs');
 const path = require('path');
 
-const sourceCode = fs.readFileSync(path.join(__dirname, './sourceCode.js'), {
+const sourceCode = fs.readFileSync(path.join(__dirname, './sourceCode.tsx'), {
     encoding: 'utf-8'
 });
 
@@ -16,7 +17,11 @@ const ast = parser.parse(sourceCode, {
 const { code } = transformFromAstSync(ast, sourceCode, {
     plugins: [[autoI18nPlugin, {
         outputDir: path.resolve(__dirname, './output')
-    }]]
+    }]],
+    presets: ["@babel/preset-typescript", {
+        isTSX: true,
+        allExtensions: true
+    }],
 });
 
 console.log(code);
--- babel-plugin-exercize/exercize-auto-i18n/src/sourceCode.tsx
+++ babel-plugin-exercize/exercize-auto-i18n/src/sourceCode.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-unused-vars */
 import intl from 'intl2';
 /**
  * App
