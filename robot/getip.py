afc729a0f5ea6c595522fe8085e9118224680946
lei hehl <lei.hehl@shopee.com>
2022-05-19 16:29:11 +0800

    python

---
+++ robot/getip.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*- 
+
+import socket
+
+def get_host_ip():
+    """
+    查询本机ip地址
+    :return: ip
+    """
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+
+    return ip
+
+if __name__ == '__main__':
+    print(get_host_ip())
+
---
+++ robot/hello.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3.10
+# -*- coding: UTF-8 -*- 
+# name = input('please enter your name: ')
+# print('hello,', name)
+
+
+# a = 100
+# if a > 0:
+#   print(a)
+# else:
+#   print(-a)
+
+# aee = int(input("请输入您的年龄第一个数字："))
+# ape = int(input("请输入您的年龄第一个数字："))
+# age = aee*10 + ape
+# if age > 60:    
+#   print("你已经老了都",age,"岁啦！")
+# elif age > 30:    
+#   print("恭喜你正值中年")
+# else:    
+#   print("还年轻")
+
+print(True and True)
+print(True or True)
+print(not 0)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+
+classmates = [1, 2, 3]
+print(len(classmates))
+print(classmates[0])
+classmates.append(4);
+print(len(classmates))
+t = (1,)
+m = (1, 2)
+print(t[0])
+print(m)
